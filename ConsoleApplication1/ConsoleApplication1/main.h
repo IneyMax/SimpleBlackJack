52c9e87c83e10ba755218a90e69c5d59461a345a
IneyMax <mr.algree@gmail.com>
2021-09-10 16:45:32 +0300

    Так, то есть жать коммит чтобы что-то добавлять и Push чтобы отправлять

--- ConsoleApplication1/ConsoleApplication1/main.h
+++ ConsoleApplication1/ConsoleApplication1/main.h
@@ -4,7 +4,8 @@ int add(int a,int b);
 
 namespace dream_team
 {
-    std::string vova {"Vova"};
+    std::string vova {"Change2"};
+    std::string sova {"Change3"};
     std::string sasha {"Change"};
     std::string maxim {"Max"};
     
